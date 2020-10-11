e4d233a23d7240c8c0dc4f38fd0b2f4d238eff8a
Nikita Kasatkin <niksonkk44@gmail.com>
2020-10-11 16:26:31 +0300

    initial commit

---
+++ predict_husky.py
@@ -0,0 +1,148 @@
+from __future__ import print_function
+import argparse
+import skimage
+import skimage.io
+import skimage.transform
+from PIL import Image
+from math import log10
+#from GCNet.modules.GCNet import L1Loss
+import sys
+import shutil
+import os
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+from torch.autograd import Variable
+from torch.utils.data import DataLoader
+#from models.GANet_deep import GANet
+from dataloader.data import get_test_set
+import numpy as np
+from tqdm import tqdm
+from time import time
+
+# Training settings
+parser = argparse.ArgumentParser(description='PyTorch GANet Example')
+parser.add_argument('--crop_height', type=int, required=True, help="crop height")
+parser.add_argument('--crop_width', type=int, required=True, help="crop width")
+parser.add_argument('--max_disp', type=int, default=192, help="max disp")
+parser.add_argument('--resume', type=str, default='', help="resume from saved model")
+parser.add_argument('--cuda', type=bool, default=True, help='use cuda?')
+parser.add_argument('--kitti', type=int, default=0, help='kitti dataset? Default=False')
+parser.add_argument('--kitti2015', type=int, default=0, help='kitti 2015? Default=False')
+parser.add_argument('--left_path', type=str, required=True, help="data root")
+parser.add_argument('--right_path', type=str, required=True, help="data root")
+parser.add_argument('--save_path', type=str, default='./result/', help="location to save result")
+parser.add_argument('--model', type=str, default='GANet_deep', help="model to train")
+
+opt = parser.parse_args()
+
+
+print(opt)
+if opt.model == 'GANet11':
+    from models.GANet11 import GANet
+elif opt.model == 'GANet_deep':
+    from models.GANet_deep import GANet
+else:
+    raise Exception("No suitable model found ...")
+    
+cuda = opt.cuda
+#cuda = True
+if cuda and not torch.cuda.is_available():
+    raise Exception("No GPU found, please run without --cuda")
+
+#torch.manual_seed(opt.seed)
+#if cuda:
+#    torch.cuda.manual_seed(opt.seed)
+#print('===> Loading datasets')
+
+
+print('===> Building model')
+model = GANet(opt.max_disp)
+
+if cuda:
+    model = torch.nn.DataParallel(model).cuda()
+
+if opt.resume:
+    if os.path.isfile(opt.resume):
+        print("=> loading checkpoint '{}'".format(opt.resume))
+        checkpoint = torch.load(opt.resume)
+        model.load_state_dict(checkpoint['state_dict'], strict=False)
+       
+    else:
+        print("=> no checkpoint found at '{}'".format(opt.resume))
+
+
+def test_transform(temp_data, crop_height, crop_width):
+    _, h, w = np.shape(temp_data)
+
+    cx, cy = w // 2, h // 2
+    hwcrop, hhcrop = crop_width // 2, crop_height // 2
+    temp_data = temp_data[:, cy - hhcrop:cy + hhcrop, cx - hwcrop:cx + hwcrop]
+
+    left = np.ones([1, 3, crop_height, crop_width], 'float32')
+    left[0, :, :, :] = temp_data[0: 3, :, :]
+    right = np.ones([1, 3, crop_height, crop_width], 'float32')
+    right[0, :, :, :] = temp_data[3: 6, :, :]
+    return torch.from_numpy(left).float(), torch.from_numpy(right).float(), h, w
+
+def load_data(leftname, rightname):
+    left = Image.open(leftname)
+    right = Image.open(rightname)
+    size = np.shape(left)
+    height = size[0]
+    width = size[1]
+    temp_data = np.zeros([6, height, width], 'float32')
+    left = np.asarray(left)
+    right = np.asarray(right)
+    r = left[:, :, 0]
+    g = left[:, :, 1]
+    b = left[:, :, 2]
+    temp_data[0, :, :] = (r - np.mean(r[:])) / np.std(r[:])
+    temp_data[1, :, :] = (g - np.mean(g[:])) / np.std(g[:])
+    temp_data[2, :, :] = (b - np.mean(b[:])) / np.std(b[:])
+    r = right[:, :, 0]
+    g = right[:, :, 1]
+    b = right[:, :, 2]  
+    #r,g,b,_ = right.split()
+    temp_data[3, :, :] = (r - np.mean(r[:])) / np.std(r[:])
+    temp_data[4, :, :] = (g - np.mean(g[:])) / np.std(g[:])
+    temp_data[5, :, :] = (b - np.mean(b[:])) / np.std(b[:])
+    return temp_data
+
+def test(leftname, rightname, savename):
+  #  count=0
+    
+    input1, input2, height, width = test_transform(load_data(leftname, rightname), opt.crop_height, opt.crop_width)
+
+    
+    input1 = Variable(input1, requires_grad = False)
+    input2 = Variable(input2, requires_grad = False)
+
+    model.eval()
+    if cuda:
+        input1 = input1.cuda()
+        input2 = input2.cuda()
+    with torch.no_grad():
+        # t = time()
+        prediction = model(input1, input2)
+        # print(f'Prediction got {time() - t:4f}s.')
+     
+    temp = prediction.cpu()
+    temp = temp.detach().numpy()
+    if height <= opt.crop_height and width <= opt.crop_width:
+        temp = temp[0, opt.crop_height - height: opt.crop_height, opt.crop_width - width: opt.crop_width]
+    else:
+        temp = temp[0, :, :]
+    skimage.io.imsave(savename, (temp * 256).astype('uint16'))
+
+   
+if __name__ == "__main__":
+    left_images = [os.path.join(opt.left_path, filename) for filename in sorted(os.listdir(opt.left_path))]
+    right_images = [os.path.join(opt.right_path, filename) for filename in sorted(os.listdir(opt.right_path))]
+    os.makedirs(opt.save_path, exist_ok=True)
+
+    for leftname, rightname in tqdm(zip(left_images, right_images)):
+        savename = os.path.join(opt.save_path, os.path.basename(leftname))
+        test(leftname, rightname, savename)
